b14174d23ee9e3dac798879cb84f6f8db0c45ec1
David Rey <neisserrey@upeu.edu.pe>
2025-09-16 19:14:40 +0000

    feat: :sparkles: Custom exceptions

---
+++ app/api/exceptions.py
@@ -0,0 +1,31 @@
+from api_exception import BaseExceptionCode
+
+
+class CustomExceptionCode(BaseExceptionCode):
+    """Excepciones personalizadas para el API."""
+
+    GET_SESSION_ERROR = (
+        "API-504",
+        "SOAP Fault en GetSession.",
+        "Error al conectar con el servicio SIS.",
+    )
+    DISCONECTED_SIS_SERVICE = (
+        "API-503",
+        "Error al conectar con el servicio SIS.",
+        "Porfavor intente mas tarde.",
+    )
+    BAD_RESPONSE = (
+        "API-422",
+        "Respuesta del servicio invalida.",
+        "Porfavor intente mas tarde.",
+    )
+    INVALID_CREDENTIALS = (
+        "API-401",
+        "Credenciales inválidas",
+        "Porfavor intente mas tarde.",
+    )
+    PERMISSION_DENIED = (
+        "PERM-403",
+        "Permission denied.",
+        "Access to this resource is forbidden.",
+    )
