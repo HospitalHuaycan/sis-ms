0e03063eb53ee60b26dfcc0d8b4e87a0efcae332
David Rey <neisserrey@upeu.edu.pe>
2025-09-19 20:23:21 +0000

    fix: :ambulance: Todos los campos a str.

--- app/models/afiliado.py
+++ app/models/afiliado.py
@@ -6,7 +6,7 @@ class Afiliado(BaseModel):
 
     IdError: int
     Resultado: str
-    TipoDocumento: int | None = None
+    TipoDocumento: str | None = None
     NroDocumento: str | None = None
     ApePaterno: str | None = None
     ApeMaterno: str | None = None
@@ -16,21 +16,21 @@ class Afiliado(BaseModel):
     DescEESS: str | None = None
     EESSUbigeo: str | None = None
     DescEESSUbigeo: str | None = None
-    Regimen: int | None = None
+    Regimen: str | None = None
     TipoSeguro: str | None = None
     DescTipoSeguro: str | None = None
     Contrato: str | None = None
     FecCaducidad: str | None = None
     Estado: str | None = None
-    Tabla: int | None = None
-    IdNumReg: int | None = None
-    Genero: int | None = None
+    Tabla: str | None = None
+    IdNumReg: str | None = None
+    Genero: str | None = None
     FecNacimiento: str | None = None
-    IdUbigeo: int | None = None
-    Disa: int | None = None
-    TipoFormato: int | None = None
+    IdUbigeo: str | None = None
+    Disa: str | None = None
+    TipoFormato: str | None = None
     NroContrato: str | None = None
     Correlativo: str | None = None
-    IdPlan: int | None = None
-    IdGrupoPoblacional: int | None = None
+    IdPlan: str | None = None
+    IdGrupoPoblacional: str | None = None
     MsgConfidencial: str | None = None
